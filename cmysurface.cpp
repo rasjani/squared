50eb12ce36468607eca19b0fbcc2f33dc96b5c9e
j11mikko <ext-jani.3.mikkonen@nokia.com>
2009-06-22 04:03:16 +0300

    foo

--- cmysurface.cpp
+++ cmysurface.cpp
@@ -1,4 +1,5 @@
 #include "csurface.h"
+#include <iostream>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 
@@ -8,7 +9,8 @@ CSurface::CSurface() :
 }
 
 CSurface::CSurface(char *filename) :
-        name("")
+        name(""),
+        image(0)
 {
     load(filename);
 }
@@ -66,3 +68,28 @@ bool CSurface::draw(CSurface* dest, int x, int y) {
     return false;
 }
 
+
+bool CSurface::drawPartial(CSurface *dest, int x, int y, int destX, int destY, int w, int h) {
+    if (dest != 0) {
+        return drawPartial(dest->getImage(),x,y,destX,destY,w,h);
+    }
+    return false;
+}
+
+bool CSurface::drawPartial(SDL_Surface *dest, int x, int y, int destX, int destY, int w, int h) {
+    if ( dest != 0 || image != 0) {
+        SDL_Rect d;
+        SDL_Rect s;
+        d.x = x;
+        d.y = y;
+
+        s.x = destX;
+        s.y = destY;
+        s.h = h;
+        s.w = w;
+
+        SDL_BlitSurface(image, &s, dest, &d);
+        return true;
+    }
+    return false;
+}
