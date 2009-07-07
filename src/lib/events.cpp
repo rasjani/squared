e9b80ca237eb88ede5953a2b3012f29a752300ae
jani mikkonen <jani.mikkonen@gmail.com>
2009-07-07 19:04:29 +0300

    New abstraction to handling events

---
+++ src/lib/events.cpp
@@ -0,0 +1,299 @@
+#include "events.h"
+#include "support.h"
+
+
+Events::Events() {
+
+}
+
+Events::~Events() {
+
+
+}
+
+void Events::onEvent(SDL_Event *Event) {
+    switch(Event->type) {
+        case SDL_ACTIVEEVENT: {
+            switch(Event->active.state) {
+                case SDL_APPMOUSEFOCUS: {
+                    if ( Event->active.gain )   
+                        onMouseFocus();
+                    else
+                        onMouseBlur();
+                    break;
+                }
+                case SDL_APPINPUTFOCUS: {
+                    if ( Event->active.gain )   
+                        onInputFocus();
+                    else
+                        onInputBlur();
+
+                    break;
+                }
+                case SDL_APPACTIVE: {
+                    if ( Event->active.gain )   
+                        onRestore();
+                    else                
+                        onMinimize();
+                    break;
+                }
+            }
+            break;
+        }
+
+        case SDL_KEYDOWN: {
+            onKeyDown(Event->key.keysym.sym,Event->key.keysym.mod,Event->key.keysym.unicode);
+            break;
+        }
+
+        case SDL_KEYUP: {
+            onKeyUp(Event->key.keysym.sym,Event->key.keysym.mod,Event->key.keysym.unicode);
+            break;
+        }
+
+        case SDL_MOUSEMOTION: {
+            onMouseMove(Event->motion.x,
+                            Event->motion.y,
+                            Event->motion.xrel,
+                            Event->motion.yrel,
+                            (Event->motion.state&SDL_BUTTON(SDL_BUTTON_LEFT))!=0,
+                            (Event->motion.state&SDL_BUTTON(SDL_BUTTON_RIGHT))!=0,
+                            (Event->motion.state&SDL_BUTTON(SDL_BUTTON_MIDDLE))!=0
+            );
+            break;
+        }
+
+        case SDL_MOUSEBUTTONDOWN: {
+            switch(Event->button.button) {
+                case SDL_BUTTON_LEFT: {
+                    onLButtonDown(Event->button.x,Event->button.y);
+                    break;
+                }
+                case SDL_BUTTON_RIGHT: {
+                    onRButtonDown(Event->button.x,Event->button.y);
+                    break;
+                }
+                case SDL_BUTTON_MIDDLE: {
+                    onMButtonDown(Event->button.x,Event->button.y);
+                    break;
+                }
+            }
+            break;
+        }
+
+        case SDL_MOUSEBUTTONUP: {
+            switch(Event->button.button) {
+                case SDL_BUTTON_LEFT: {
+                    onLButtonUp(Event->button.x,Event->button.y);
+                    break;
+                }
+                case SDL_BUTTON_RIGHT: {
+                    onRButtonUp(Event->button.x,Event->button.y);
+                    break;
+                }
+                case SDL_BUTTON_MIDDLE: {
+                    onMButtonUp(Event->button.x,Event->button.y);
+                    break;
+                }
+            }
+            break;
+        }
+
+        case SDL_JOYAXISMOTION: {
+            onJoyAxis(Event->jaxis.which,Event->jaxis.axis,Event->jaxis.value);
+            break;
+        }
+
+        case SDL_JOYBALLMOTION: {
+            onJoyBall(Event->jball.which,Event->jball.ball,Event->jball.xrel,Event->jball.yrel);
+            break;
+        }
+
+        case SDL_JOYHATMOTION: {
+            onJoyHat(Event->jhat.which,Event->jhat.hat,Event->jhat.value);
+            break;
+        }
+        case SDL_JOYBUTTONDOWN: {
+            onJoyButtonDown(Event->jbutton.which,Event->jbutton.button);
+            break;
+        }
+
+        case SDL_JOYBUTTONUP: {
+            onJoyButtonUp(Event->jbutton.which,Event->jbutton.button);
+            break;
+        }
+
+        case SDL_QUIT: {
+            onExit();
+            break;
+        }
+
+        case SDL_SYSWMEVENT: {
+            //Ignore
+            break;
+        }
+
+        case SDL_VIDEORESIZE: {
+            onResize(Event->resize.w,Event->resize.h);
+            break;
+        }
+
+        case SDL_VIDEOEXPOSE: {
+            onExpose();
+            break;
+        }
+
+        default: {
+            onUser(Event->user.type,Event->user.code,Event->user.data1,Event->user.data2);
+            break;
+        }
+    }
+
+}
+
+void Events::onInputFocus() {
+    //Pure virtual, do nothing
+}
+
+void Events::onInputBlur() {
+    //Pure virtual, do nothing
+}
+
+void Events::onKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) {
+    //Pure virtual, do nothing
+    UNUSED(sym);
+    UNUSED(mod);
+    UNUSED(unicode);
+}
+
+void Events::onKeyUp(SDLKey sym, SDLMod mod, Uint16 unicode) {
+    //Pure virtual, do nothing
+    UNUSED(sym);
+    UNUSED(mod);
+    UNUSED(unicode);
+}
+
+void Events::onMouseFocus() {
+    //Pure virtual, do nothing
+}
+
+void Events::onMouseBlur() {
+    //Pure virtual, do nothing
+}
+
+void Events::onMouseMove(int mX, int mY, int relX, int relY, bool Left,bool Right,bool Middle) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+    UNUSED(relX);
+    UNUSED(relY);
+    UNUSED(Left);
+    UNUSED(Right);
+    UNUSED(Middle);
+}
+
+void Events::onMouseWheel(bool Up, bool Down) {
+    //Pure virtual, do nothing
+    UNUSED(Up);
+    UNUSED(Down);
+}
+
+void Events::onLButtonDown(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onLButtonUp(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onRButtonDown(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onRButtonUp(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onMButtonDown(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onMButtonUp(int mX, int mY) {
+    //Pure virtual, do nothing
+    UNUSED(mX);
+    UNUSED(mY);
+}
+
+void Events::onJoyAxis(Uint8 which,Uint8 axis,Sint16 value) {
+    //Pure virtual, do nothing
+    UNUSED(which);
+    UNUSED(axis);
+    UNUSED(value);
+}
+
+void Events::onJoyButtonDown(Uint8 which,Uint8 button) {
+    //Pure virtual, do nothing
+    UNUSED(which);
+    UNUSED(button);
+}
+
+void Events::onJoyButtonUp(Uint8 which,Uint8 button) {
+    //Pure virtual, do nothing
+    UNUSED(which);
+    UNUSED(button);
+}
+
+void Events::onJoyHat(Uint8 which,Uint8 hat,Uint8 value) {
+    //Pure virtual, do nothing
+    UNUSED(which);
+    UNUSED(hat);
+    UNUSED(value);
+}
+
+void Events::onJoyBall(Uint8 which,Uint8 ball,Sint16 xrel,Sint16 yrel) {
+    //Pure virtual, do nothing
+    UNUSED(which);
+    UNUSED(ball);
+    UNUSED(xrel);
+    UNUSED(yrel);
+}
+
+void Events::onMinimize() {
+    //Pure virtual, do nothing
+}
+
+void Events::onRestore() {
+    //Pure virtual, do nothing
+}
+
+void Events::onResize(int w,int h) {
+    //Pure virtual, do nothing
+    UNUSED(w);
+    UNUSED(h);
+}
+
+void Events::onExpose() {
+    //Pure virtual, do nothing
+}
+
+void Events::onExit() {
+    //Pure virtual, do nothing
+}
+
+void Events::onUser(Uint8 type, int code, void* data1, void* data2) {
+    //Pure virtual, do nothing
+    UNUSED(type);
+    UNUSED(code);
+    UNUSED(data1);
+    UNUSED(data2);
+}
---
+++ src/lib/events.h
@@ -0,0 +1,58 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+
+#include <SDL/SDL.h>
+
+/**
+ * @brief Events ... short description ...
+ * @author Jani Mikkonen <ext-jani.3.mikkonen@nokia.com>
+ * @date 2009-07-07
+ * ... description ...
+ */
+
+class Events
+{
+  public:
+
+    /**
+     * Default constructor
+     */
+    Events();
+
+    /**
+     * Destructor
+     */
+    virtual ~Events();
+
+    virtual void onEvent(SDL_Event* Event);
+    virtual void onInputFocus();
+    virtual void onInputBlur();
+    virtual void onKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode);
+    virtual void onKeyUp(SDLKey sym, SDLMod mod, Uint16 unicode);
+    virtual void onMouseFocus();
+    virtual void onMouseBlur();
+    virtual void onMouseMove(int mX, int mY, int relX, int relY, bool Left,bool Right,bool Middle);
+    virtual void onMouseWheel(bool Up, bool Down);  //Not implemented
+    virtual void onLButtonDown(int mX, int mY);
+    virtual void onLButtonUp(int mX, int mY);
+    virtual void onRButtonDown(int mX, int mY);
+    virtual void onRButtonUp(int mX, int mY);
+    virtual void onMButtonDown(int mX, int mY);
+    virtual void onMButtonUp(int mX, int mY);
+    virtual void onJoyAxis(Uint8 which, Uint8 axis, Sint16 value);
+    virtual void onJoyButtonDown(Uint8 which, Uint8 button);
+    virtual void onJoyButtonUp(Uint8 which, Uint8 button);
+    virtual void onJoyHat(Uint8 which, Uint8 hat, Uint8 value);
+    virtual void onJoyBall(Uint8 which, Uint8 ball, Sint16 xrel, Sint16 yrel);
+    virtual void onMinimize();
+    virtual void onRestore();
+    virtual void onResize(int w,int h);
+    virtual void onExpose();
+    virtual void onExit();
+    virtual void onUser(Uint8 type, int code, void* data1, void* data2);
+
+  private:
+
+};
+
+#endif /* #ifndef EVENTS_H */
